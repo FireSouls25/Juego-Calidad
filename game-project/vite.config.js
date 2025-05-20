022c2035cda080692957cb2f7186c82e60601815
Enmanuel <david.castillofl@campuesucc.edu.co>
2025-05-20 03:00:17 -0500

    Vercel

--- game-project/vite.config.js
+++ game-project/vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom', 'three'],
+          'physics': ['cannon-es', 'cannon-es-debugger'],
+          'ui': ['lil-gui', 'gsap']
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'three', 'cannon-es', 'cannon-es-debugger', 'lil-gui', 'gsap']
+  }
 })
