6d52d0641d7fcf28102bbf8be0cdc40adb52aacd
Enmanuel <david.castillofl@campuesucc.edu.co>
2025-05-20 03:07:46 -0500

    Vercel

--- game-project/vite.config.js
+++ game-project/vite.config.js
@@ -18,6 +18,6 @@ export default defineConfig({
   },
   optimizeDeps: {
     include: ['react', 'react-dom', 'three', 'cannon-es', 'cannon-es-debugger', 'lil-gui', 'gsap']
-  }
+  },
   base: './',
 })
